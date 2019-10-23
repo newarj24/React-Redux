bc8258bb420386ee9c3dfba6afcfdd0ba6e0a55f
newarj24 <newar013@gmail.com>
2019-10-23 16:57:45 +0530

    Add and Edit course form functionality added

--- src/components/courses/CourseList.js
+++ src/components/courses/CourseList.js
@@ -1,48 +1,56 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Spinner from "../common/Spinner";
 
 const CourseList = ({ courses, onDeleteClick }) => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th />
-        <th>Title</th>
-        <th>Author</th>
-        <th>Category</th>
-        <th />
-      </tr>
-    </thead>
-    <tbody>
-      {courses.map(course => {
-        return (
-          <tr key={course.id}>
-            <td>
-              <a
-                className="btn btn-light"
-                href={"http://pluralsight.com/courses/" + course.slug}
-              >
-                Watch
-              </a>
-            </td>
-            <td>
-              <Link to={"/course/" + course.slug}>{course.title}</Link>
-            </td>
-            <td>{course.authorName}</td>
-            <td>{course.category}</td>
-            <td>
-              <button
-                className="btn btn-outline-danger"
-                onClick={() => onDeleteClick(course)}
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        );
-      })}
-    </tbody>
-  </table>
+  <React.Fragment>
+    <Link to="course" className="btn btn-primary mb-5">
+      Add Course
+    </Link>
+
+    <Spinner />
+    <table className="table">
+      <thead>
+        <tr>
+          <th />
+          <th>Title</th>
+          <th>Author</th>
+          <th>Category</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {courses.map(course => {
+          return (
+            <tr key={course.id}>
+              <td>
+                <a
+                  className="btn btn-light"
+                  href={"http://pluralsight.com/courses/" + course.slug}
+                >
+                  Watch
+                </a>
+              </td>
+              <td>
+                <Link to={"/course/" + course.slug}>{course.title}</Link>
+              </td>
+              <td>{course.authorName}</td>
+              <td>{course.category}</td>
+              <td>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => onDeleteClick(course)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  </React.Fragment>
 );
 
 CourseList.propTypes = {
--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import propTypes from "prop-types";
 
 import { connect } from "react-redux";
-import { loadCourses } from "../../redux/actions/courseActions";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 
 import CourseForm from "./CourseForm";
@@ -14,8 +14,13 @@ function ManageCoursePage({
   authors,
   loadCourses,
   loadAuthors,
+  saveCourse,
+  history,
   ...props
 }) {
+  // Above line will create an issue : formfield will not populate the result after the reloading the edit course
+  // as data is not available while page is rendering
+  // We can solve this by tweaking useEffect
   const [course, setCourse] = useState({ ...props.course });
   const [errors, setErrors] = useState({});
 
@@ -24,6 +29,10 @@ function ManageCoursePage({
       loadCourses().catch(error => {
         alert("Loading courses failed!!", error);
       });
+    } else {
+      setCourse({
+        ...props.course
+      });
     }
 
     if (authors.length === 0) {
@@ -31,14 +40,47 @@ function ManageCoursePage({
         alert("Loading authors failed!!", error);
       });
     }
-  }, []);
+  }, [props.course]);
 
-  return <CourseForm course={course} errors={errors} authors={authors} />;
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse(prevCourse => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value
+    }));
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course).then(() => {
+      history.push("/courses");
+    }); // saveCourse is this.props.saveCourse getting from function argument not the imported one.
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSave}
+    />
+  );
 }
 
-function mapStateToProps(state) {
+export function getCourseBySlug(courses, slug) {
+  return courses.find(course => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
+
   return {
-    course: newCourse,
+    course,
     courses: state.courses,
     authors: state.authors
   };
@@ -46,7 +88,8 @@ function mapStateToProps(state) {
 
 const mapDispatchToProps = {
   loadCourses,
-  loadAuthors
+  loadAuthors,
+  saveCourse
 };
 
 ManageCoursePage.propTypes = {
@@ -54,7 +97,9 @@ ManageCoursePage.propTypes = {
   courses: propTypes.array.isRequired,
   authors: propTypes.array.isRequired,
   loadCourses: propTypes.func.isRequired,
-  loadAuthors: propTypes.func.isRequired
+  loadAuthors: propTypes.func.isRequired,
+  saveCourse: propTypes.func.isRequired,
+  history: propTypes.object.isRequired
 };
 
 export default connect(
